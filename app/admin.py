b9152abe4a66d81b91da5c7f658295057e45dbee
mnx758 <roogiyura2009@gmail.com>
2024-04-07 19:44:56 +0300

    beta version 1.0

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from app.models import Games
 @admin.register(Games)
 class Games_admin(admin.ModelAdmin):
-    list_display=['title','description','slug','price','created_time','update_time','publisher','color']
+    list_display=['title','description','slug','price','created_time','update_time','publisher']
